65bb79437953fd62a9aced91fd4e7f5181fd91cd
jeungHunLee <ksjs1111@gmail.com>
2023-09-29 22:39:26 +0900

    FEAT:0929 add BeaconRepository fetching the Beacon UUID from FireStore

--- CheckPass/Repositories/BeaconsRepository.swift
+++ CheckPass/Repositories/BeaconsRepository.swift
@@ -5,4 +5,27 @@
 //  Created by 이정훈 on 9/28/23.
 //
 
-import Foundation
+import Combine
+import Firebase
+
+struct BeaconsRepository {
+    static private let db = Firestore.firestore()
+    
+    static func fetchBeaconUUIDList() -> AnyPublisher<[String], Error> {
+        return Future { promise in
+            db.collection("BEACONS").getDocuments { (querySnapshot, error) in
+                if let error = error {
+                    promise(.failure(error))
+                } else {
+                    //Beacon UUID List
+                    let documentList = querySnapshot!.documents.map {
+                        $0.documentID
+                    }
+                    
+                    promise(.success(documentList))
+                }
+            }
+        }
+        .eraseToAnyPublisher()
+    }
+}

5320cf3c0db05e21804938e31f0085b248c885ba
jeungHunLee <ksjs1111@gmail.com>
2023-11-06 22:00:20 +0900

    FEAT::1106 add lecture time field

--- CheckPass/Model/Lecture.swift
+++ CheckPass/Model/Lecture.swift
@@ -14,10 +14,11 @@ struct Lecture: Codable {
     var BEACON_MAJOR: Int    //beacon major value
     var BEACON_MINOR: Int    //beacon minor value
     var BEACON_UUID: String    //beacon UUID
-    var CREDIT: String    //학점
-    var GRADE: String   //학년
+    var DETAILS: String
     var LECTURE_NAME: String    //강의명
     var LECTURE_ROOM: String    //강의실
     var PROFESSOR_NAME: String    //교수명
     var TOTAL_USER: Int    //수강 중인 총원
+    var LECTURE_TIME1: [String]
+    var LECTURE_TIME2: [String]
 }
--- CheckPass/View/BeaconAttendanceView/ScannedLectureListRowView.swift
+++ CheckPass/View/BeaconAttendanceView/ScannedLectureListRowView.swift
@@ -48,6 +48,6 @@ struct ScannedLectureListRowView: View {
 }
 
 #Preview {
-    ScannedLectureListRowView(showLectureSheet: .constant(false), lecture: Lecture(ATTENDANCE_STATUS: 1, BEACON_MAJOR: 11111, BEACON_MINOR: 22222, BEACON_UUID: "FE850A83-6660-4792-B2CF-886689B32552", CREDIT: "2", GRADE: "3", LECTURE_NAME: "캡스톤 디자인", LECTURE_ROOM: "미래융합정보관 225호", PROFESSOR_NAME: "홍길동", TOTAL_USER: 50))
+    ScannedLectureListRowView(showLectureSheet: .constant(false), lecture: Lecture(ATTENDANCE_STATUS: 1, BEACON_MAJOR: 11111, BEACON_MINOR: 22222, BEACON_UUID: "FE850A83-6660-4792-B2CF-886689B32552", DETAILS: "3학년 전필 2학점", LECTURE_NAME: "캡스톤 디자인", LECTURE_ROOM: "미래융합정보관 225호", PROFESSOR_NAME: "홍길동", TOTAL_USER: 50, LECTURE_TIME1: ["금", "1A", "1B"], LECTURE_TIME2: []))
         .environmentObject(ScannedLecturesViewModel())
 }

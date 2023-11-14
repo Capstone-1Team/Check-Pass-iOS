6a2f61789eb3f6ae28cf8c5b7529e8f017bdc64d
jeungHunLee <ksjs1111@gmail.com>
2023-11-14 15:57:42 +0900

    DESIGN::1114 change Image

--- CheckPass/View/EAttendanceView/NoLectureView.swift
+++ CheckPass/View/EAttendanceView/NoLectureView.swift
@@ -9,7 +9,24 @@ import SwiftUI
 
 struct NoLectureView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Image("no_lecture")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 200, height: 200)
+                .padding(.bottom)
+            
+            Text("앗...")
+                .font(.title)
+                .bold()
+                .padding(.bottom, 10)
+            
+            Text("강의 리스트가 존재하지 않아요")
+            
+            Text("메인 화면 → 강의 탭에서 강의를 추가 해주세요")
+        }
+        .offset(y: -50)
+        .foregroundColor(.gray)
     }
 }
 
